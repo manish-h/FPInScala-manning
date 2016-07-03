53f97b7f1e340a15828b6cdbf4b79e4bb2471213
manishh <hotwani.manish@gmail.com>
2016-07-03 22:37:36 +0530

    Chapter 3 exercises - continued

--- src/chap3/List.scala
+++ src/chap3/List.scala
@@ -49,18 +49,92 @@ object ListRunner extends App {
     // Ex 3.5
     def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
       case Nil         => Nil
-      case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else Cons(x, xs)
+      case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
     }
+
+    // Ex 3.6
+    def init[A](l: List[A]): List[A] = l match {
+      case Nil                   => Nil
+      case Cons(x, Cons(y, Nil)) => Cons(x, Nil)
+      case Cons(x, xs)           => Cons(x, init(xs))
+    }
+
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+      case Nil         => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+    // Ex 3.9
+    def length[A](as: List[A]): Int = foldRight(as, 0)((_, b) => b + 1)
+
+    // Ex 3.10
+    def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+
+      @tailrec
+      def loop(as: List[A], acc: B): B = as match {
+        case Nil         => acc
+        case Cons(x, xs) => loop(xs, f(acc, x))
+      }
+
+      loop(as, z)
+    }
+
+    // Ex 3.11
+    def sumUsingFoldLeft(list: List[Int]): Int = foldLeft(list, 0)(_ + _)
+
+    def lengthUsingFoldLeft[A](list: List[A]): Int = foldLeft(list, 0)((a: Int, b: A) => a + 1)
+
+    // Ex 3.12
+    def reverse[A](list: List[A]): List[A] = foldLeft(list, Nil: List[A])((xs, x) => Cons(x, xs))
+
+    // Ex 3.14
+    def append[A](left: List[A], right: List[A]): List[A] = foldRight(left, right)(Cons(_, _))
+
+    // Ex 3.15
+    def concat[A](lists: List[List[A]]): List[A] = foldRight(lists, Nil: List[A])(append)
+
+    // Ex 3.16
+    def addOne(l: List[Int]): List[Int] = foldRight(l, Nil: List[Int])((x, xs) => Cons(x + 1, xs))
+
+    // Ex 3.17
+    def doubleToString(l: List[Double]): List[String] = foldRight(l, Nil: List[String])((x, xs) => Cons(x.toString, xs))
+
+    // Ex 3.18
+    def map[A, B](as: List[A])(f: A => B): List[B] = foldRight(as, Nil: List[B])((x, xs) => Cons(f(x), xs))
+
+    // Ex 3.19
+    def filter[A](as: List[A])(f: A => Boolean): List[A] = foldRight(as, Nil: List[A])((x, xs) => if (f(x)) Cons(x, xs) else xs)
+
+    // Ex 3.20
+    def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = concat(map(as)(f))
+
+    // Ex 3.21
+    def filterUsingFlatMap[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)(a => if (f(a)) List(a) else Nil)
+    
+    // Ex 3.23
+//    def zipWith[A,B,C](a: List[A], b: List[B])(f: (A,B)=>C): List[C] = 
 
   }
 
   val list = List(1, 3, 4, 7, 5)
 
-  println(evaluateX())
-  println(List.tail(list))
-  println(List.setHead(list, 5))
-  println(List.drop(list, 2))
-  println(List.dropWhile(list, (i: Int) => i < 5))
+  println("evaluateX " + evaluateX())
+  println("tail " + List.tail(list))
+  println("setHead " + List.setHead(list, 5))
+  println("drop " + List.drop(list, 2))
+  println("dropWhile " + List.dropWhile(list, (i: Int) => i < 5))
+  println("init " + List.init(list))
+  println("length " + List.length(list))
+  println("sum using foldLeft " + List.sumUsingFoldLeft(list))
+  println("length using foldLeft " + List.lengthUsingFoldLeft(list))
+  println("reverse using foldLeft " + List.reverse(list))
+  println("append " + List.append(list, list))
+  println("concat " + List.concat(List(list, list, list)))
+  println("add  1 " + List.addOne(list))
+  println("double to string " + List.doubleToString(List(1.3, 3.3)))
+  println("map " + List.map(list)(_ * 2))
+  println("filter " + List.filter(list)(_ > 4))
+  println("flatMap " + List.flatMap(list)(a => Cons(a, Cons(a * a, Nil))))
 
   // Ex 3.1
   def evaluateX(): Int = {
@@ -72,4 +146,5 @@ object ListRunner extends App {
       case _                                     => 101
     }
   }
+
 }
